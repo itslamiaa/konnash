09bedca7f0a4d3546ad4cc36a2f17ced2b8a5e3c
Meguireche Lamia <meguirechelamia@gmail.com>
2026-04-26 12:47:41 +0100

    add customer ui fixed

--- app/src/main/java/com/example/konnashapp/CategoryActivity.java
+++ app/src/main/java/com/example/konnashapp/CategoryActivity.java
@@ -2,6 +2,7 @@ package com.example.konnashapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,11 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_category);
         View pickColorBtn = findViewById(R.id.pickColorBtn);
+        ImageView closeBtn = findViewById(R.id.closeBtn);
+
+        closeBtn.setOnClickListener(v -> {
+            finish();
+        });
 
         pickColorBtn.setOnClickListener(v -> {
             ColorPicker sheet = new ColorPicker(color -> {
--- app/src/main/java/com/example/konnashapp/CustomerActivity.java
+++ app/src/main/java/com/example/konnashapp/CustomerActivity.java
@@ -1,18 +1,43 @@
 package com.example.konnashapp;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.location.Address;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.hbb20.CountryCodePicker;
 
 public class CustomerActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_customer);
+        CountryCodePicker countryCodePicker = findViewById(R.id.ccp);
+        EditText phoneNumber = findViewById(R.id.phoneNumber);
+
+        countryCodePicker.registerCarrierNumberEditText(phoneNumber);
+
+        Button addCategory = findViewById(R.id.add_category_button);
+        ImageView addressBtn = findViewById(R.id.btnAddress);
+
+        addressBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(CustomerActivity.this , AddressActivity.class);
+            startActivity(intent);
+        });
+
+        addCategory.setOnClickListener(v -> {
+            Intent intent = new Intent(CustomerActivity.this, CategoryActivity.class);
+            startActivity(intent);
+        });
 
 
     }
